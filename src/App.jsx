9589955e26a07305e60d4a1e56e6357c8f6efbff
Radu Brehar <radu@jslog.com>
2024-04-16 16:09:01 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -3,64 +3,109 @@ import { AgGridReact } from "ag-grid-react"; // AG Grid Component
 import "ag-grid-community/styles/ag-grid.css"; // Mandatory CSS required by the grid
 import "ag-grid-community/styles/ag-theme-quartz.css"; // Optional Theme applied to the grid
 
-function PriceCellRenderer(params) {
-  // console.log(params);
-  return (
-    <div style={{ width: params.rowIndex % 100 }}>
-      <span>
-        <input defaultValue={params.value}></input>!!!
-      </span>
-    </div>
-  );
+const genColumns = [
+  ...Array.from({ length: 100 }, (_, i) => ({
+    field: `col-${i}`,
+  })),
+];
+
+const columns = [
+  { field: "id" },
+  { field: "make" },
+  { field: "model" },
+  { field: "price" },
+
+  // generate 100 more columns
+  ...genColumns,
+];
+
+function addColPropsToObject(obj) {
+  return genColumns.reduce((acc, col, i) => {
+    acc[`col-${i}`] = `${col.field} - ${obj.id}`;
+    return acc;
+  }, obj);
 }
 
-const getRowId = (data) => data.id;
-const GridExample = () => {
-  // Row Data: The data to be displayed.
-  const [rowData, setRowData] = useState([
-    { make: "Tesla", model: "Model Y", price: 64950, electric: true, id: "x" },
-    { make: "Ford", model: "F-Series", price: 33850, electric: false, id: "y" },
-    {
-      make: "Toyota",
-      model: "Corolla",
-      price: 29600,
-      electric: false,
-      id: "z",
-    },
-    // generate another 1000 rows
-    ...Array.from({ length: 1000 }, (_, i) => ({
+const rowData = [
+  addColPropsToObject({
+    make: "Tesla",
+    model: "Model Y",
+    price: 64950,
+    electric: true,
+    id: "x",
+  }),
+  addColPropsToObject({
+    make: "Ford",
+    model: "F-Series",
+    price: 33850,
+    electric: false,
+    id: "y",
+  }),
+  addColPropsToObject({
+    make: "Toyota",
+    model: "Corolla",
+    price: 29600,
+    electric: false,
+    id: "z",
+  }),
+  // generate another 1000 rows
+  ...Array.from({ length: 1000 }, (_, i) =>
+    addColPropsToObject({
       make: `Make ${i}`,
       model: `Model ${i}`,
       id: `id-${i}`,
       price: Math.floor(Math.random() * 100000),
       electric: Math.random() > 0.5,
-    })),
-  ]);
+    })
+  ),
+];
 
-  // Column Definitions: Defines the columns to be displayed.
-  const [colDefs, setColDefs] = useState([
-    { field: "id" },
-    { field: "make" },
-    { field: "model" },
-    // { field: "price", cellRenderer: PriceCellRenderer },
-    { field: "price" },
-    { field: "electric" },
-  ]);
+const getRowId = (data) => data.id;
+
+function getColumns() {
+  const cols = [...columns];
+  const count = localStorage.getItem("columnCount") * 1 || 4;
+  cols.length = count;
+  return cols;
+}
+
+const GridExample = () => {
+  const [colDefs, setColDefs] = useState(getColumns);
+
+  const setColumnCount = (value) => {
+    localStorage.setItem("columnCount", value);
+    setColDefs(getColumns());
+  };
 
   return (
-    // wrapping container with theme & size
-    <div
-      className="ag-theme-quartz" // applying the grid theme
-      style={{ height: "500px", width: "100vw" }} // the grid will fill the size of the parent container
-    >
-      <AgGridReact
-        getRowId={getRowId}
-        rowData={rowData}
-        columnDefs={colDefs}
-        rowHeight={18}
-        rowBuffer={0}
-        ensureDomOrder={false}
-      />
+    <div>
+      Grid height: <b>500px</b>. Row height: <b>18px</b>. Row count{" "}
+      <b>{rowData.length}</b>. Column count{" "}
+      <select
+        value={localStorage.getItem("columnCount") || 4}
+        onChange={(event) => {
+          setColumnCount(event.target.value);
+        }}
+      >
+        <option value="4">4</option>
+        <option value="10">10</option>
+        <option value="20">20</option>
+        <option value="50">50</option>
+        <option value="100">100</option>
+      </select>
+      <div
+        className="ag-theme-quartz" // applying the grid theme
+        style={{ height: "500px", width: "100vw" }} // the grid will fill the size of the parent container
+      >
+        <AgGridReact
+          getRowId={getRowId}
+          rowData={rowData}
+          columnDefs={colDefs}
+          rowHeight={18}
+          rowBuffer={0}
+          ensureDomOrder={false}
+        />
+      </div>
     </div>
   );
 };
